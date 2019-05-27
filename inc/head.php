4a9ac7b1cf8457436c378e0044eb3eff56a09a5c
adi <adityabhat@lazaro.in>
2019-05-27 16:19:15 +0530

    Google Site Verification : Add the code

--- inc/head.php
+++ inc/head.php
@@ -81,7 +81,7 @@ $pageTitle = getCurrentPageTitle( $links, $baseURL, $siteTitle );
 	<meta name="robots" content="index,follow"><!-- All Search Engines -->
 	<meta name="googlebot" content="index,follow"><!-- Google Specific -->
 	<!-- Verify website ownership -->
-	<meta name="google-site-verification" content="<?php echo getContent( '', 'google_site_verification_token' ); ?>"><!-- Google Search Console -->
+	<meta name="google-site-verification" content="<?php echo getContent( 'gkvpBPv3gaRIpwXVIUaTjbQcT3x_Df9bqBoxl6oTZSA', 'google_site_verification_token' ); ?>"><!-- Google Search Console -->
 
 
 	<!--
